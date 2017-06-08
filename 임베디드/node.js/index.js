df41dc00f9431c47a06efb29eb416a04f5f61d3e
leeyun1533 <leeyun1533@naver.com>
2017-06-08 22:01:49 +0900

    web_complete

--- 임베디드/node.js/index.js
+++ 임베디드/node.js/index.js
@@ -16,7 +16,9 @@ app.get('/act',(req,res)=>{
 	console.log("actuator (led) 호출");
 	fs.readFile('test.jade','utf8',(error,data)=>{
 	const fn=jade.compile(data);
-	res.send(fn());
+	res.send(fn({name:'myname'}));
+	
+	
 	});
 });
 
@@ -29,11 +31,36 @@ app.get('/act/0',(req,res)=>{
 	console.log("led OFF 호출");
 	res.redirect('/act');
 });
+app.get('/act/sense_fire',(req,res)=>{
+        console.log("actuator (led) 호출");
+        fs.readFile('sense_fire.jade','utf8',(error,data)=>{
+        const fn=jade.compile(data);
+        res.send(fn({name:'myname'}));
+
+
+        });
+});
+app.get('/act/instrusion_Detection',(req,res)=>{
+        console.log("actuator (led) 호출");
+        fs.readFile('instrusion_Detection.jade','utf8',(error,data)=>{
+        const fn=jade.compile(data);
+        res.send(fn({name:'myname'}));
+
+
+        });
+});
+app.get('/act/setting',(req,res)=>{
+        console.log("actuator (led) 호출");
+        fs.readFile('setting.jade','utf8',(error,data)=>{
+        const fn=jade.compile(data);
+        res.send(fn({name:'myname'}));
+
+
+        });
+});
 
 app.listen(60001,()=>{
 	console.log("웹서핑실행중(http://192.168.0.4:60001");
 	console.log("http://IP주소:60001/act  또는");
 	console.log("http://IP주소:60001/sen");
 });
-
-
